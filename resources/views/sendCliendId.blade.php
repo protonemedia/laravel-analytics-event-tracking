23f340e88baae636e53c39cd256292fd5c910dd1
Pascal Baljet <pascal@protone.media>
2021-01-14 10:51:42 +0100

    Update sendCliendId.blade.php

--- resources/views/sendCliendId.blade.php
+++ resources/views/sendCliendId.blade.php
@@ -1,7 +1,15 @@
-ga(function () {
-    var clientId = ga.getAll()[0].get('clientId');
+if (typeof ga !== 'undefined') {
+    ga(function () {
+        var clientId = ga.getAll()[0].get('clientId');
 
-    if(clientId != @json(app('analytics-event-tracking.client-id'))) {
-        window.axios.post('/gaid', { id: clientId });
-    }
-});
+        if (clientId != @json(app('analytics-event-tracking.client-id'))) {
+            window.axios.post('/gaid', { id: clientId });
+        }
+    });
+} else {
+    gtag('get', @json(config('analytics-event-tracking.tracking_id')), 'client_id', function (clientId) {
+        if (clientId != @json(app('analytics-event-tracking.client-id'))) {
+            window.axios.post('/gaid', { id: clientId });
+        }
+    });
+}
